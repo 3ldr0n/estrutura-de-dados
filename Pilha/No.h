e60a626287d55d8ad39b84acafb3e84059a95b41
Edison Neto <ednetoali@gmail.com>
2019-05-21 18:38:53 -0300

    Adiciona o No para a lista ligada simples

---
+++ Pilha/No.h
@@ -0,0 +1,16 @@
+#ifndef NO_H
+#define NO_H
+
+template <class E>
+class ListaLigada;
+
+template <class E>
+class No {
+private:
+    E elem;
+    No<E> *prox;
+    friend class ListaLigada<E>;
+    friend class Polinomio;
+};
+
+#endif
