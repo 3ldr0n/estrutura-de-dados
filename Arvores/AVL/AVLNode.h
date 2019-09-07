b9474da8860cfd17682d64074c2a0666dd25c1f8
Edison Neto <ednetoali@gmail.com>
2019-09-07 17:46:48 -0300

    Arvore AVL

---
+++ Arvores/AVL/AVLNode.h
@@ -0,0 +1,24 @@
+#ifndef AVLNODE_H
+#define AVLNODE_H
+
+class AVLNode
+{
+public:
+    AVLNode();
+    AVLNode(int);
+    virtual ~AVLNode();
+    int getData();
+    int getHeight();
+    void setData(int);
+    void setHeight(int);
+    AVLNode* getLeft();
+    AVLNode* getRight();
+    void setRight(AVLNode*);
+    void setLeft(AVLNode*);
+private:
+    AVLNode* left, *right;
+    int data;
+    int height;
+};
+
+#endif
